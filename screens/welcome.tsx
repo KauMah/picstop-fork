1ae170770620c00775d946044f8caafe17a72133
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2020-12-27 17:26:39 -0500

    added fonts

--- screens/welcome.tsx
+++ screens/welcome.tsx
@@ -1,13 +1,27 @@
-import {Image, SafeAreaView, Text, View} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 import React from 'react';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 28,
+    lineHeight: 34,
+    fontFamily: 'Kumbh Sans',
+    fontWeight: 'bold',
+  },
+});
 
 const Welcome = () => {
   return (
     <View>
       <SafeAreaView>
         <Image source={require('../assets/picstop-logo.png')} />
-        <Text>Henlo</Text>
+        <Text style={styles.headerText}>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit
+        </Text>
       </SafeAreaView>
     </View>
   );
