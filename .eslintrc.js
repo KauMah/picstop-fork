f2f52fda2a158c09fe64d1be028daca2a306ef83
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2021-01-24 00:46:19 -0500

    another test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,18 @@
 module.exports = {
-    root: true,
-    extends: '@react-native-community',
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'unused-imports'],
+  rules: {
+    'unused-imports/no-unused-imports-ts': 'error',
+    'unused-imports/no-unused-vars-ts': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
