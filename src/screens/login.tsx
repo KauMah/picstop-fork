2cd8f3a3f961f7fafb548e13b53abfdf505f1fff
nicholasmdoherty <nicholasmdoherty@gmail.com>
2021-01-26 12:49:55 -0500

    add KeyboardAvoidingView to login & signup

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
 
 import { $white } from '../utils/colors';
 // @ts-ignore: Weirdness with react-native-dotenv
@@ -85,64 +85,65 @@ const Login = () => {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Image
-        source={require('../../assets/img/picstop-logo.png')}
-        style={styles.logo}
-      />
-      <Formik
-        initialValues={{ username: '', password: '' }}
-        onSubmit={postSignIn}
-        initialErrors={{ username: 'err', password: 'err' }}
-        validationSchema={LogInSchema}>
-        {({
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          values,
-          errors,
-          touched,
-          isValid,
-        }) => (
-          <View style={styles.inputs}>
-            <IconTextField
-              icon={faUser}
-              placeholder={'Username'}
-              value={values.username}
-              onBlur={handleBlur('username')}
-              onChangeText={handleChange('username')}
-              style={styles.textField}
-              autoCapitalize={'none'}
-              invalid={
-                touched.username ? (errors.username ? true : false) : false
-              }
-            />
-            <IconTextField
-              icon={faLock}
-              placeholder={'Password'}
-              value={values.password}
-              onBlur={handleBlur('password')}
-              onChangeText={handleChange('password')}
-              style={styles.textField}
-              invalid={
-                touched.password ? (errors.password ? true : false) : false
-              }
-              secureTextEntry
-            />
-            <View style={styles.button}>
-              <StyledButton
-                text={'Sign In'}
-                type="green"
-                onPress={handleSubmit}
-                disabled={!touched || !isValid}
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "position" : undefined} >
+      <SafeAreaView style={styles.container}>
+        <Image
+          source={require('../../assets/img/picstop-logo.png')}
+          style={styles.logo}
+        />
+        <Formik
+          initialValues={{ username: '', password: '' }}
+          onSubmit={postSignIn}
+          initialErrors={{ username: 'err', password: 'err' }}
+          validationSchema={LogInSchema}>
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+            isValid,
+          }) => (
+            <View style={styles.inputs}>
+              <IconTextField
+                icon={faUser}
+                placeholder={'Username'}
+                value={values.username}
+                onBlur={handleBlur('username')}
+                onChangeText={handleChange('username')}
+                style={styles.textField}
+                autoCapitalize={'none'}
+                invalid={
+                  touched.username ? (errors.username ? true : false) : false
+                }
               />
+              <IconTextField
+                icon={faLock}
+                placeholder={'Password'}
+                value={values.password}
+                onBlur={handleBlur('password')}
+                onChangeText={handleChange('password')}
+                style={styles.textField}
+                invalid={
+                  touched.password ? (errors.password ? true : false) : false
+                }
+                secureTextEntry
+              />
+              <View style={styles.button}>
+                <StyledButton
+                  text={'Sign In'}
+                  type="green"
+                  onPress={handleSubmit}
+                  disabled={!touched || !isValid}
+                />
+              </View>
             </View>
-          </View>
-        )}
-      </Formik>
-
-      <Text style={styles.forgot}>Forgot your password? Reset password</Text>
-    </SafeAreaView>
+          )}
+        </Formik>
+        <Text style={styles.forgot}>Forgot your password? Reset password</Text>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
