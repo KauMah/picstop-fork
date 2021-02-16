ae1090190b2f10551026e41c3684764e750287af
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2021-02-16 08:57:34 -0500

    functionality

--- src/components/Search/SearchItem/component.tsx
+++ src/components/Search/SearchItem/component.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { Location, User } from '../../../types';
 
 import { $white } from '../../../utils/colors';
 import React from 'react';
@@ -8,21 +9,58 @@ const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: $white,
     height: 40,
-    marginVertical: 1,
+    marginVertical: 2,
     borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  pic: {
+    height: 30,
+    width: 30,
+    margin: 5,
+    borderRadius: 5,
+  },
+  text: {
+    fontFamily: 'Kumbh Sans',
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginLeft: 10,
   },
 });
 interface SearchItemProps {
   type: 'user' | 'location';
   text: string;
+  picUrl?: string;
+  user?: User;
+  location?: Location;
 }
+// navigation.navigate('Location', { locationId: props.location?._id })
+// navigation.navigate('userProfile', { userId: props.user?._id })
 
 const SearchItem: React.FC<SearchItemProps> = (props) => {
-  return (
-    <View style={styles.container}>
-      <Text>{props.text}</Text>
-    </View>
-  );
+  // const navigation = useNavigation();
+  switch (props.type) {
+    case 'location':
+      return (
+        <View style={styles.container}>
+          <Text style={styles.text}>{props.text}</Text>
+        </View>
+      );
+    case 'user':
+      return (
+        <View style={styles.container}>
+          {props.picUrl !== '' ? (
+            <Image style={styles.pic} source={{ uri: props.picUrl }} />
+          ) : (
+            <Image
+              style={styles.pic}
+              source={require('../../../../assets/img/picstop-no-text.png')}
+            />
+          )}
+          <Text style={styles.text}>{props.text}</Text>
+        </View>
+      );
+  }
 };
 
 export default SearchItem;
